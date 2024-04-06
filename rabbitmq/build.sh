1a7fccfc337cfed896108039091b9849665f5826
wangzhongyang <979238315@qq.com>
2024-04-06 21:03:21 +0800

    4444

--- rabbitmq/build.sh
+++ rabbitmq/build.sh
@@ -2,9 +2,9 @@
 set -x
 
 # 定义镜像名称和标签  
-IMAGE_NAME="pyflink"  
+IMAGE_NAME="rabbitmq-with-delayed"  
 TAG="v$(date +"%Y%m%d")"
-FILE_PATH=$(pwd)/PyFlink/Dockerfile
+FILE_PATH=$(pwd)/rabbitmq/Dockerfile
 REGISTRY="979238315"
 
 . $(pwd)/public/public.sh

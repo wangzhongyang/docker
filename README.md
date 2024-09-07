fbbae1a70121f44b6beaa85b18f93b4c4e279ac8
wangzhongyang <979238315@qq.com>
2024-09-07 07:50:27 +0000

    update frpc

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ## 主要用途
 存放一些Dockerfile文件
+
+## 使用示例
+```shell
+make build-frps -e VERSION=0.60.0
+```
--- frpc/build.sh
+++ frpc/build.sh
@@ -3,8 +3,8 @@ set -x
   
 # 定义镜像名称和标签  
 IMAGE_NAME="frpc"  
-TAG="v0.60.0"  
-FILE_PATH=$(pwd)/docker/frpc/Dockerfile
+# TAG="v0.60.0"  
+FILE_PATH=$(pwd)/frpc/Dockerfile
 REGISTRY="979238315"
 
 . $(pwd)/public/public.sh

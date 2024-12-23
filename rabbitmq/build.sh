145dbac80539ba4f0829e36f1c7727d38eb93962
wangzhongyang <979238315@qq.com>
2024-12-23 06:25:04 +0000

    udpate rabbitmq to 4.0.5

--- rabbitmq/build.sh
+++ rabbitmq/build.sh
@@ -3,12 +3,12 @@ set -x
 
 # 定义镜像名称和标签  
 IMAGE_NAME="rabbitmq-with-delayed"  
-VERSION="3.13.1"
+VERSION="4.0.5"
 FILE_PATH=$(pwd)/rabbitmq/Dockerfile
 REGISTRY="979238315"
 
-wget https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/v3.13.0/rabbitmq_delayed_message_exchange-3.13.0.ez
+wget https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/v4.0.2/rabbitmq_delayed_message_exchange-4.0.2.ez
 
 . $(pwd)/public/public.sh
 
-rm rabbitmq_delayed_message_exchange-3.13.0.ez
+rm rabbitmq_delayed_message_exchange-4.0.2.ez
